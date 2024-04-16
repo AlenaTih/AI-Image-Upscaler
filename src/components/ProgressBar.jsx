affcaff0bacbc84fa8908c1a26d74c8ff54b2ae0
Alena <alyonatihomirova9@gmail.com>
2024-04-16 16:18:46 +0300

    Update

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,12 +1,21 @@
 import React from "react"
 import "./ProgressBar.css"
 
+// function ProgressBar(progress) {
+//     return (
+//         <div className="progress-bar-container">
+//             <div className="progress-bar" style={{ width: `${progress}%`}}>
+//                 <p>{`${progress}%`}</p>
+//             </div>
+//             {console.log(progress)}
+//         </div>
+//     )
+// }
+
 function ProgressBar(progress) {
     return (
         <div className="progress-bar-container">
-            <div className="progress-bar" style={{ width: `${progress}%`}}>
-                <p>{`${progress}%`}</p>
-            </div>
+            <div className="progress-bar"></div>
             {console.log(progress)}
         </div>
     )
