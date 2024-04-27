e425b2af3e1fa6e8c9204988b50e0504ffca0deb
Alena <alyonatihomirova9@gmail.com>
2024-04-27 16:56:44 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -541,7 +541,7 @@ useEffect(() => {
 
           <section className="how-it-works" id="how-it-works">
             <div className="how-it-works-left">
-            <h1>How It Works</h1>
+            <h1 className="how-it-works-title">How It Works</h1>
             <p>Image Upscaler is an online service that zooms images and photos from 2 to 6 times. 
               It uses artificial intelligence that enlarges images without loss of quality, 
               making them clearer.</p>
--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -4,7 +4,7 @@ function Authors() {
     return (
         <>
             <div className="authors-left">
-              <h1>Authors</h1>
+              <h1 className="authors-title">Authors</h1>
               <p>Image Upscaler is an online service that zooms images and photos from 2 to 6 times. 
                 It uses artificial intelligence that enlarges images without loss of quality, 
                 making them clearer.</p>
