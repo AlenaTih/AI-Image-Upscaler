181cf44e7b884f6970e61f7b45f1a1717d42091c
Alena <alyonatihomirova9@gmail.com>
2024-05-03 16:53:41 +0300

    Update

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,28 +1,3 @@
-// import React from 'react'
-// import DemoImageCat from "../assets/demo-image-cat.png";
-// import Sunset from "../assets/sunset.png";
-// import Coffee from "../assets/coffee.png";
-// import CoffeeLaptop from "../assets/coffee-laptop.png";
-// import NatureLover from "../assets/nature-lover.png";
-// import Spring from "../assets/spring.png";
-// import Succulent from "../assets/succulent.png";
-// import SustainableStay from "../assets/sustainable-stay.png";
-
-// function Tab({ index, active, onClick }) {
-//   return (
-//     <div
-//       className={`tab ${active ? 'active' : ''}`}
-//       onClick={() => onClick(index)}
-//     >
-//       {/* <img src={DemoImageCat}></img> */}
-//       Tab {index + 1}
-//     </div>
-//   )
-// }
-
-// export default Tab
-
-
 import React from 'react';
 
 function Tab({ index, active, onClick, imagePaths }) {
@@ -43,5 +18,4 @@ function Tab({ index, active, onClick, imagePaths }) {
   );
 }
 
-export default Tab;
-
+export default Tab
