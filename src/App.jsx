868ed57734c4d0e0f9057e040ef1b7365e86681e
Alena <alyonatihomirova9@gmail.com>
2024-05-11 00:14:16 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -90,8 +90,6 @@ function App() {
     console.log(file.name.split(".")[0])
     const newFileName = file.name.split(".")[0]
 
-    alert(file.name)
-
     console.log(file.name.split(".")[1])
     const newOriginalFormat = file.name.split(".")[1]
 
