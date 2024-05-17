0c3a2705336c15cb3c91eaf1850cd9285e3c9c5f
Alena <alyonatihomirova9@gmail.com>
2024-05-17 03:36:20 +0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import { Helmet } from "react-helmet"
 import App from './App.jsx'
 import './index.css'
 
   function Main() {
     return (
+      // <>
+      // <Helmet>
+      //   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      // </Helmet>
       <div className="app">
         <App />
       </div>
+      // </>
     )
   }
 
