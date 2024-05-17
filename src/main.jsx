07190e009d0b67ce0504ec115c161eb0889d2277
Alena <alyonatihomirova9@gmail.com>
2024-05-18 01:25:19 +0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { Helmet } from "react-helmet"
+import { Helmet } from "react-helmet"
 import App from './App.jsx'
 import './index.css'
 
   function Main() {
     return (
-      // <>
-      // <Helmet>
-      //   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      // </Helmet>
-      <div className="app">
-        <App />
-      </div>
-      // </>
+      <>
+        <Helmet>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Helmet>
+        <div className="app">
+          <App />
+        </div>
+      </>
     )
   }
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Main />
+//   </React.StrictMode>,
+// )
 
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(<Main />)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Main />)
