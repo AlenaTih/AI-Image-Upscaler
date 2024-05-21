5507c4122cb4f428b687f847d9fe9654ce63c6cf
Alena <alyonatihomirova9@gmail.com>
2024-05-22 02:16:39 +0300

    Update

--- src/components/DemoContainer.jsx
+++ src/components/DemoContainer.jsx
@@ -10,6 +10,8 @@ import NatureLover from "../assets/nature-lover.png";
 import Spring from "../assets/spring.png";
 import Succulent from "../assets/succulent.png";
 import SustainableStay from "../assets/sustainable-stay.png";
+import Allien from "../assets/allien.png"
+import ElfGirl from "../assets/elf-girl.png"
 
 function DemoContainer() {
   const [activeTab, setActiveTab] = useState(0);
@@ -20,12 +22,11 @@ function DemoContainer() {
 
   // Array of image paths for each tab
   const imagePaths = [
-    [DemoImageCat, DemoImageCat], // Tab 1
-    [Coffee, CoffeeLaptop], // tab 2
-    [Spring, Spring],   
-    [SustainableStay, SustainableStay]
-    // Add more image pairs for other tabs as needed
-  ];
+    [DemoImageCat, DemoImageCat],
+    [ElfGirl, ElfGirl],
+    [Coffee, CoffeeLaptop], 
+    [Allien, Allien]
+  ]
 
   return (
     <div className="demo-container">
