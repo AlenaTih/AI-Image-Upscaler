65969284150ba7d037eb1078959dd8305a24d80e
Alena <alyonatihomirova9@gmail.com>
2024-05-26 13:35:56 +0300

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,25 +10,33 @@ function Footer() {
     const currentUrl = window.location.href // The URL of the current page
     const shareText = encodeURIComponent("Check out this awesome project!")
 
-    const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?url=${encodeURIComponent(currentUrl)}&p[summary]=${shareText}`
-    const twitterShareUrl = `https://x.com/intent/tweet?url=${encodeURIComponent(currentUrl)}&text=${shareText}`
+    // const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?url=${encodeURIComponent(currentUrl)}&p[summary]=${shareText}`
+    // const twitterShareUrl = `https://x.com/intent/tweet?url=${encodeURIComponent(currentUrl)}&text=${shareText}`
     const linkedinShareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(currentUrl)}&text=${shareText}`
 
-    // href="http://www.facebook.com/sharer.php?s=100&p[title]=YOUR_TITLE&p[summary]=YOUR_SUMMARY&p[url]=YOUR_URL&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT"
+    const handleFacebookShare = () => {
+        window.open(facebookShareUrl, "_blank")
+    }
+
+    const handleTwitterShare = () => {
+        window.open(twitterShareUrl, "_blank")
+    }
+
+    const handleLinkedinShare = () => {
+        window.open(linkedinShareUrl, "_blank")
+    }
 
     return (
         <>
             <div className="footer-title-container">
-                <h2 className="footer-title">{getDynamicYear()} &copy; Image Upscaler</h2>
+                <h2 className="footer-title">{getDynamicYear()} &copy Image Upscaler</h2>
             </div>
 
             <div className="footer-navbar">
-
                 <h3 className="footer-share">Share</h3>
-                <a className="footer-social" href={facebookShareUrl} target="_blank" rel="noopener noreferrer">Facebook</a>
-                <a className="footer-social" href={twitterShareUrl} target="_blank" rel="noopener noreferrer">X (Twitter)</a>
-                <a className="footer-social" href={linkedinShareUrl} target="_blank" rel="noopener noreferrer">LinkedIn</a>
-
+                <button className="footer-social" onClick={handleFacebookShare}>Facebook</button>
+                <button className="footer-social" onClick={handleTwitterShare}>X (Twitter)</button>
+                <button className="footer-social" onClick={handleLinkedinShare}>LinkedIn</button>
             </div>
         </>
     )
